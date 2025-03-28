d21f60fa9e0162464f0fd679901b14634baff702
Gaurav Singh <kanwasigaurav96@gmail.com>
2025-03-28 22:21:37 +0530

    Update and rename config.ini to config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+
+CAMERA_INDEX = 0  # Default camera index
+SMOOTHING_FACTOR = 0.5  # For cursor movement smoothing
+CLICK_THRESHOLD = 0.1  # Distance threshold for click gesture
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 480
